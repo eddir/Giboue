24fe3372a21d21629a585069382395462dfb9cb7
Eduard Rostkov <eddirworkmail@gmail.com>
2018-09-18 16:38:53 +0200

    Мониторинг работы сайта и отчёт по посещаемости раз в сутки

--- Main.py
+++ Main.py
@@ -1,2 +1,36 @@
+import inspect
+import os
+import sys
+from datetime import datetime
+from shutil import copyfile
+
+import yaml
+
+import site_analyzer
+
+
+def get_path():
+    return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+
 if __name__ == "__main__":
-    print("Hello")
+    if not os.path.exists(get_path() + '/config.yml'):
+        copyfile(get_path() + '/resources/config.yml', get_path() + '/config.yml')
+    try:
+        with open(get_path() + '/config.yml', 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+    except (OSError, IOError):
+        sys.exit('Ошибка чтения файла конфигурации config.yml')
+
+    now = datetime.now()
+
+    for sc in cfg['sites']:
+        site = site_analyzer.Site(sc)
+        if sc["check-ping"]:
+            site.check_ping()
+        if sc["check-content"]:
+            site.check_content()
+        if sc["check-errors"]:
+            site.check_errors()
+        if sc["report"] == str(now.hour) + ":" + str(now.minute):
+            site.generate_report()
---
+++ site_analyzer.py
@@ -0,0 +1,119 @@
+import os
+import sys
+from datetime import date, timedelta
+
+import requests
+import telegram as telegram
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+import Main
+
+
+def get_service(api_name, api_version, scopes, key_file_location):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        key_file_location, scopes=scopes)
+
+    # Build the service object.
+    service = build(api_name, api_version, credentials=credentials)
+
+    return service
+
+
+class Site:
+
+    def __init__(self, config):
+        self.cfg = config
+        self.content = ""
+        if not os.path.isfile(Main.get_path() + '/client_secrets.json'):
+            sys.exit('Не найден необходимый файл client_secrets.json')
+        self.service = get_service(
+            api_name='analytics',
+            api_version='v3',
+            scopes=['https://www.googleapis.com/auth/analytics.readonly'],
+            key_file_location=Main.get_path() + '/client_secrets.json'
+        )
+        self.bot = telegram.Bot(token=config["telegram"]["token"])
+        self.response = requests.get(self.cfg["address"])
+        self.prohibited_words = ("Warning", "PHP", "MYSQL")
+
+    def check_ping(self):
+        if self.response.status_code != 200:
+            self.telegram_send(
+                "*Обнаружена неисправность*\n\n_Сайт не отвечает. Код HTTP ошибки:_ " +
+                str(self.response.status_code))
+            self.telegram_report()
+
+    def check_content(self):
+        if len(self.response.content) < 40000:
+            self.telegram_send("*Обнаружена неисправность*\n\n_Подозрительный вывод на сайте_")
+            self.telegram_report()
+
+    def check_errors(self):
+        content = self.response.content.decode('UTF-8')
+        for word in self.prohibited_words:
+            if content.find(word) != -1:
+                self.telegram_send("*Обнаружена неисправность*\n\n_Ошибки в алгоритме_" + word)
+                self.telegram_report()
+
+    def telegram_report(self):
+        document = open("report.txt", "wb")
+        document.write(self.response.content)
+        document.close()
+        document = open("report.txt", "rb")
+        self.bot.send_document(chat_id=self.cfg["telegram"]["group"],
+                               document=document,
+                               filename="Crash_" + self.cfg["address"] + "_" + date.today().strftime('%y%m%d') + ".txt")
+        document.close()
+        os.remove("report.txt")
+        sys.exit()
+
+    def telegram_send(self, message):
+        print(message)
+        self.bot.send_message(chat_id=self.cfg["telegram"]["group"], text=message, parse_mode="Markdown")
+
+    def generate_report(self):
+        yesterday = (date.today() - timedelta(1)).strftime('%y%m%d')
+        visitors = self.service.data().ga().get(
+            ids=self.cfg["google-analytics"]["table-id"],
+            start_date=self.cfg['google-analytics']['from'],
+            end_date=self.cfg['google-analytics']['to'],
+            dimensions='ga:date',
+            metrics='ga:visitors'
+        ).execute()
+        maxv = visitors['rows'][-1]
+        minv = visitors['rows'][-1]
+        for day in visitors['rows']:
+            if day[1] > maxv[1]:
+                maxv = day
+            if day[1] < minv[1]:
+                minv = day
+        if maxv[0] == yesterday:
+            self.telegram_send("Вчера было зафиксировано самое высокое количество посетителей за 2 недели: " + maxv[1])
+        if minv[0] == yesterday:
+            self.telegram_send("Вчера было зафиксировано самое низкое количество посетителей за 2 недели: " + maxv[1])
+
+        visits = self.service.data().ga().get(
+            ids=self.cfg["google-analytics"]["table-id"],
+            start_date=self.cfg['google-analytics']['from'],
+            end_date=self.cfg['google-analytics']['to'],
+            dimensions='ga:date',
+            metrics='ga:visits'
+        ).execute()
+        maxv = visits['rows'][-1]
+        minv = visits['rows'][-1]
+        for day in visits['rows']:
+            if day[1] > maxv[1]:
+                maxv = day
+            if day[1] < minv[1]:
+                minv = day
+        if maxv[0] == yesterday:
+            self.telegram_send(
+                "Вчера было зафиксировано самое высокое количество просмотров за 2 недели: *"
+                + maxv[1] + "*")
+        if minv[0] == yesterday:
+            self.telegram_send(
+                "Вчера было зафиксировано самое низкое количество просмотров за 2 недели: *"
+                + maxv[1] + "*")
+
+        self.telegram_send("Посетители: *" + visitors['rows'][-1][1] + "*\nПросмотры: *" + visits['rows'][-1][1] + "*")
